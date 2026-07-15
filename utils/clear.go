55b71258985035827f144e000dbefe8bf39fbba4
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-07-15 16:42:49 +0700

    feat: add util clear terminal

---
+++ utils/clear.go
@@ -0,0 +1,12 @@
+package utils
+
+import "fmt"
+
+func Clear() { 
+  // Source - https://stackoverflow.com/a/22892171
+  // Posted by Kavu, modified by community. See post 'Timeline' for change history
+  // Retrieved 2026-07-15, License - CC BY-SA 4.0
+  
+  fmt.Print("\033[H\033[2J")
+
+}
